8108926cc9010a0053255dc864c291245064d874
kritishaaa <kritishamoktan530@gmail.com>
2023-09-15 21:21:02 +0545

    connection done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const path = require('path');
+const express= require('express');
+
+const app= express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT=4000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
